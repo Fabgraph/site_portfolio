0cddf16faf2b1ac0254002fb9bd35da872987c6b
Fabrice Domoison <39194172+Fabgraph@users.noreply.github.com>
2018-10-17 16:41:30 +0200

    commit13
    c13

--- admin/inc/footer.php
+++ admin/inc/footer.php
@@ -5,7 +5,6 @@
   <!-- Copyright -->
   <div class="footer-copyright text-center py-3 text-info">© 2018 Copyright:
     <a href="https://mdbootstrap.com/bootstrap-tutorial/" class="text-primary"> MDBootstrap.com</a>
-    <a href="https://fr.freepik.com/vecteurs-libre/abstrait-avec-des-points-et-des-lignes_2348138.htm">Designed by Freepik</a>
   </div>
   <!-- Copyright -->
 
